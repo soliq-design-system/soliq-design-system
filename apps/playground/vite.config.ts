415507669132e1994b30463a59832ffd79cbdf27
XasanDeveloper <mirkhasan96@yandex.ru>
2026-06-17 11:42:53 +0500

    deploy

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]"
+      }
+    }
+  }
 });
